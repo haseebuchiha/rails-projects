e2978f5d3439e4ea775e5c23f5408c6a022e04a0
Haseeb Arshad <haseeb9009@gmail.com>
2020-12-02 16:16:07 +0500

    Update and edit routes for rails

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -9,6 +9,11 @@ class ArticlesController < ApplicationController
     end 
 
     def new 
+        @article = Article.new
+    end
+
+    def edit 
+        @article = Article.find(params[:id])
     end
 
     def create
@@ -16,8 +21,21 @@ class ArticlesController < ApplicationController
         @article = Article.new(params.require(:article).permit(:title, :description))
         # byebug
         # render plain: @article.inspect
-        puts @article.save
-        redirect_to @article
+        if @article.save
+            flash[:notice] = "Article was created kys"
+            redirect_to @article
+        else    
+            render 'new'
+        end 
+    end 
 
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
     end 
 end
